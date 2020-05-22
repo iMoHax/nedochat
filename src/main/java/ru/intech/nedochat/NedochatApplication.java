5647babe3f1d084da64b4097096fc24cb31f700f
Mo <born19th@gmail.com>
2020-05-23 00:48:29 +0300

    init repo

---
+++ src/main/java/ru/intech/nedochat/NedochatApplication.java
@@ -0,0 +1,13 @@
+package ru.intech.nedochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NedochatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NedochatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/intech/nedochat/NedochatApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.intech.nedochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NedochatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
